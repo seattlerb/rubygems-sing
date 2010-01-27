4091e96759fc68ed1198d7080fe6f4fb9dd5cf12
Ryan Davis <ryand@zenspider.com>
2010-01-26 22:17:34 -0800

    + Added blues scale and duration based on distance of ends. (topFunkay) [git-p4: depot-paths = "//src/rubygems-sing/dev/": change = 5627]

--- lib/rubygems/commands/sing_command.rb
+++ lib/rubygems/commands/sing_command.rb
@@ -3,6 +3,7 @@ require 'rubygems/version_option'
 require 'rubygems/text'
 
 require 'midiator'
+require 'English'
 
 ##
 # gem command to "sing" the implementation of a gem.
@@ -56,8 +57,10 @@ class Gem::Commands::SingCommand < Gem::Command
     midi = MIDIator::Interface.new
     midi.use :dls_synth
 
-    scale = [ C4, Cs4, D4, Eb4, E4, F4, Fs4, G4, Gs4, A4, Bb4, B4,
-              C5, Cs5, D5, Eb5, E5, F5, Fs5, G5, Gs5, A5, Bb5, B5 ]
+    # blues scale
+    scale = [ C4, Eb4, F4, Fs4, G4, Bb4,
+              C5, Eb5, F5, Fs5, G5, Bb5,
+              C6, Eb6, F6, Fs6, G6, Bb6 ]
 
     midi.control_change 32, 10, 1 # TR-808 is Program 26 in LSB bank 1
     midi.program_change 10, 26
@@ -67,10 +70,27 @@ class Gem::Commands::SingCommand < Gem::Command
     spec.lib_files.each do |path|
       full_path = File.join base, path
       warn path
+
+      line_number_of_last_end = 0
       File.foreach(full_path) do |line|
         if line =~ /^(\s+)end$/ then
+          number_of_lines_in_block = $INPUT_LINE_NUMBER - line_number_of_last_end
+          duration = case number_of_lines_in_block
+                     when  0 ..  3
+                       0.1
+                     when  4 .. 10
+                       0.25
+                     when 11 .. 30
+                       0.5
+                     else
+                       1.0
+                     end
+
+          duration *= 0.8 # tweaking for now...
+
+          line_number_of_last_end = $INPUT_LINE_NUMBER
           num_spaces = $1.size
-          midi.play scale[ num_spaces / 2 ]
+          midi.play scale[ num_spaces / 2 ], duration
           print line
         end
       end
